29eea245218a5673ce16dba74259e2c6500f3653
ore88 <52471091+ore88@users.noreply.github.com>
2022-03-12 00:55:55 -0500

    Update app.py

--- app_files/app.py
+++ app_files/app.py
@@ -13,26 +13,24 @@ FEATURES_MASK = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS',\
 
 app = Flask(__name__)
 
-@app.route('/predict',methods = ['POST'])
-def predict():
-    int_features = [float(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-    print(prediction[0])
+@app.route('/', methods=['GET'])
+def server_check():
+    return "Your First GCP Webapp Runs! Congratulations!"
 
-    #output = round(prediction[0], 2)
-    return render_template('home.html', prediction_text="Prediction {}".format(prediction[0]))
+@app.route('/predict', methods=['POST'])
+def predictor():
+    content = request.json
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+    try:
+        features = pd.DataFrame([content])
+        features = features[FEATURES_MASK]
+    except:
+        logging.exception("The JSON file was broke.")
+        return jsonify(status='error', predict=-1)
 
-    output = prediction[0]
-    return jsonify(output)
+    pred = MODEL.predict(features)[0]
+
+    return jsonify(status='ok', predict=pred)
 
 if __name__=='__main__':
     app.run( debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)) )
