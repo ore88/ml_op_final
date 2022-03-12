e713729eefbc90caeb467498989e5143128e07c4
ore88 <52471091+ore88@users.noreply.github.com>
2022-03-12 00:42:01 -0500

    Update app.py

--- app_files/app.py
+++ app_files/app.py
@@ -13,24 +13,26 @@ FEATURES_MASK = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS',\
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+@app.route('/predict',methods = ['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    print(prediction[0])
 
-@app.route('/predict', methods=['POST'])
-def predictor():
-    content = request.json
+    #output = round(prediction[0], 2)
+    return render_template('home.html', prediction_text="Prediction {}".format(prediction[0]))
 
-    try:
-        features = pd.DataFrame([content])
-        features = features[FEATURES_MASK]
-    except:
-        logging.exception("The JSON file was broke.")
-        return jsonify(status='error', predict=-1)
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
 
-    pred = MODEL.predict(features)[0]
-
-    return jsonify('home.html', predict=pred)
+    output = prediction[0]
+    return jsonify(output)
 
 if __name__=='__main__':
     app.run( debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)) )
